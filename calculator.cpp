e6807b8a527281b5f1e21b821aa38679b2fd58b3
Aaryan2332 <aarucode2332@gmail.com>
2025-08-05 17:07:28 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int a ;
+cin>>a;
+cout<<"The value of a is: " << a ;
+
+int b;
+cin>>b;
+cout<<"The value of b is: " << b ;
+
+int sum;
+cin>>sum;
+sum = a+b;
+cout <<"The sum = " <<sum<<endl;
+
+int subtraction;
+cin>>subtraction;
+subtraction = a-b;
+cout<< "The subtraction = " << subtraction << endl;
+
+int product ;
+cin>>product;
+product =a*b;
+cout<<"The product = " << product << endl;
+
+int division;
+cin>>division;
+division = a/b;
+cout<<"The division = "<<division << endl;
+
+return 0;
+}
